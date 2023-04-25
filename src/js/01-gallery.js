0740c20aa62ba139844eb380c8209b4768303d7e
KaterinaKha <katerinakhaa@gmail.com>
2023-04-26 00:26:39 +0200

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryItemElement = document.querySelector('.gallery');
+galleryItemElement.style.listStyle = 'none';
+
+galleryItemElement.insertAdjacentHTML(
+  'beforeend',
+  createGalleryItem(galleryItems)
+);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <li class='gallery__item'>
+		<a class='gallery__link' href='${original}'>
+	<img 
+      class='gallery__image' 
+      src='${preview}'
+      alt='${description}' />
+		</a>
+	</li>`;
+    })
+    .join(' ');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
 
 console.log(galleryItems);
