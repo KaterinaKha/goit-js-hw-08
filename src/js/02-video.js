afb04ecca80f17a80af18ef2864e6781eb66eeef
KaterinaKha <katerinakhaa@gmail.com>
2023-04-28 21:52:22 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+const VIDEO_CURRENT_TIME = 'videoplayer-current-time';
+
+const onPlay = function () {
+  throttle(function (data) {
+    localStorage.setItem(VIDEO_CURRENT_TIME, data.seconds);
+  }, 1000);
+};
+player.on('timeupdate', onPlay);
+const savedTime = localStorage.getItem(VIDEO_CURRENT_TIME);
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const data = {
+  email: '',
+  message: '',
+};
+
+form.addEventListener('input', throttle(inputData, 500));
+form.addEventListener('submit', submitFormHandler);
+
+const initialData = getFeedbackStateFromStorage();
+data.email = initialData.email || '';
+data.message = initialData.message || '';
+
+function inputData(e) {
+  data.email = e.target.value;
+  data.message = e.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+}
+
+function getFeedbackStateFromStorage() {
+  const feedbackStateString = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (feedbackStateString) {
+    return JSON.parse(feedbackStateString);
+  } else {
+    return {};
+  }
+}
+
+function submitFormHandler(e) {
+  e.preventDefault();
+  if (!data.email || !data.message) {
+    return alert('Будь ласка заповніть всі поля');
+  }
+
+  form.reset(data);
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  console.log(data);
+}
