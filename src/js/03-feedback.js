91ecac682586eec4cf10ebafed5bbaa2bb528095
KaterinaKha <katerinakhaa@gmail.com>
2023-05-02 13:56:33 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,25 +1,20 @@
 import throttle from 'lodash.throttle';
-
 const LOCALSTORAGE_KEY = 'feedback-form-state';
-
 const form = document.querySelector('.feedback-form');
-const data = {
+let data = {
   email: '',
   message: '',
 };
-
 form.addEventListener('input', throttle(inputData, 500));
 form.addEventListener('submit', submitFormHandler);
 
-const initialData = getFeedbackStateFromStorage();
-form['email'].value = initialData.email || '';
-form['message'].value = initialData.message || '';
-
+data = getFeedbackStateFromStorage();
+form['email'].value = data.email || '';
+form['message'].value = data.message || '';
 function inputData(e) {
   data[e.target.name] = e.target.value;
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
 }
-
 function getFeedbackStateFromStorage() {
   const feedbackStateString = localStorage.getItem(LOCALSTORAGE_KEY);
   if (feedbackStateString) {
@@ -28,14 +23,12 @@ function getFeedbackStateFromStorage() {
     return {};
   }
 }
-
 function submitFormHandler(e) {
   e.preventDefault();
   if (!data.email || !data.message) {
     return alert('Будь ласка заповніть всі поля');
   }
-
+  console.log(data);
   form.reset(data);
   localStorage.removeItem(LOCALSTORAGE_KEY);
-  console.log(data);
 }
